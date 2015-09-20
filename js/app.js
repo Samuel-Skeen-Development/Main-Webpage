c55656f7cd647e0cf59a3681603826ebacf7b427
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-09-20 17:38:41 -0600

    Update to files that are dependencies for typescript to work

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-/// <reference path="../bower_components/angular/angular.js" />
+/// <reference path="C:\\Users\\Samuel\\Documents\\Work\\main-webpage\\bower_components\\angular\\angular.js" />
 /// <reference path="maincontroller.js" />
 /// <reference path="mainservice.js" />
 
+var app = angular.module('MainApp', ['ngRoute', 'firebase', 'ngMaterial', 'ngAnimate']);
---
+++ js/app.ts
@@ -0,0 +1,7 @@
+/// <reference path="../bower_components/angular/angular.d.ts" />
+/// <reference path="maincontroller.ts" />
+/// <reference path="mainservice.ts" />
+
+var app = angular.module('MainApp', ['ngRoute', 'firebase', 'ngMaterial', 'ngAnimate']);
+
+app
