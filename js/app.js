9edc1ea74dd1b404da35b0049d7f88aaf7360dea
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-09-20 21:07:17 -0600

    Added font-awesome and got parallax to work

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 /// <reference path="../bower_components/angular/angular.d.ts" />
 /// <reference path="maincontroller.ts" />
 /// <reference path="mainservice.ts" />
-var app = angular.module('MainApp', ['ngRoute', 'firebase', 'ngMaterial', 'ngAnimate']);
+var app = angular.module('MainApp', ['ngRoute', 'firebase', 'ngMaterial', 'ngAnimate', 'perfectParallax']);
 
 app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-        $routeProvider
-                .when('/home', {
-                templateUrl: '../views/home/home.html',
-                controller: 'HomeController',
-                controllerAs: 'vm'
-                })
-                .when('/gallery', {
-                templateUrl: '../views/gallery/gallery.html',
-                controller: 'GalleryController',
-                controllerAs: 'vm'
-                })
-                .otherwise({ redirectTo: '/home' });
-        // $locationProvider.html5Mode({
-        // 	enabled: true,
-        // 	requireBase: true
-        // });
+    $routeProvider
+        .when('/home', {
+            templateUrl: '../views/home/home.html',
+            controller: 'HomeController',
+            controllerAs: 'vm'
+        })
+        .when('/gallery', {
+            templateUrl: '../views/gallery/gallery.html',
+            controller: 'GalleryController',
+            controllerAs: 'vm'
+        })
+        .otherwise({ redirectTo: '/home' });
+    // $locationProvider.html5Mode({
+    // 	enabled: true,
+    // 	requireBase: true
+    // });
 }]);
--- js/maincontroller.js
+++ js/maincontroller.js
@@ -5,6 +5,16 @@ var app = angular.module('MainApp');
 app.controller('MainController', ['$location', '$firebaseObject', '$firebaseArray', function ($location, $firebaseObject, $firebaseArray) {
     var vm = this;
     
+    vm.isMainOpen = false;
+    
+    vm.openMainMenu = function() {
+        vm.isMainOpen = true;
+    }
+    
+    vm.closeMainMenu = function() {
+        vm.isMainOpen = false;
+    }
+    
     vm.test = 'test';
     
     vm.testThis = function (data) {
