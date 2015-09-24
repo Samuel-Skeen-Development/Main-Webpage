74e73711b05fe5b209930b14a2b1b5294f59293a
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-09-24 12:25:02 -0600

    Beginning setup pub-profile tab in account settings

---
+++ js/custom-directives/account-settings tabs/pub-profile-definition.js
@@ -0,0 +1,6 @@
+app.directive('pubProfileTab', [function() {
+	return {
+		restrict: 'E',
+		templateUrl: 'js/custom-directives/account-settings tabs/pub-profile-tab.html'
+	}
+}]);
--- views/account/account-controller.js
+++ views/account/account-controller.js
@@ -1,3 +1,5 @@
 app.controller('AccountController', ['currentUser', '$location', 'Auth', function(currentUser, $location, Auth) {
 	var vm = this;
+	
+	vm.tabNum = 1;
 }]);
