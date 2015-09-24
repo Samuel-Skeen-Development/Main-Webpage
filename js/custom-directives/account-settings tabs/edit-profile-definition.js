bc986b80ea9f58e8d1aabef84f3817e0b68c6926
Sam Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2015-09-24 17:36:06 -0600

    Touching up a few things in pub-profile, beginning edit-profile

---
+++ js/custom-directives/account-settings tabs/edit-profile-definition.js
@@ -0,0 +1,31 @@
+app.directive('editProfileTab', [function() {
+	return {
+		restrict: 'E',
+		
+		templateUrl: 'js/custom-directives/account-settings tabs/edit-profile-tab.html',
+		
+		controller: ['Auth', 'MainService', '$firebaseArray', '$firebaseObject', function(Auth, MainService, $firebaseArray, $firebaseObject) {
+			var vm = this,
+				currentAuth = Auth.authObj.$getAuth(),
+				users = $firebaseArray(MainService.usersRef);
+			vm.currentUser;
+			
+			function getCurrentUser() {
+				if (currentAuth) {
+					for (var i = 0; i < users.length; i++) {
+						if (currentAuth.uid === users[i].uid) {
+							vm.currentUser = users[i];
+							break;
+						}
+					}
+				} else vm.currentUser = undefined;
+			}
+			
+			users.$loaded().then(function() {
+				getCurrentUser();
+			});
+		}],
+		
+		controllerAs: 'vm'
+	}
+}]);
